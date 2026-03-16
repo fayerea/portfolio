61bf37a18c913b376029c62467ebf50357cf640d
fayerea <fschlief@reacollege.org>
2026-03-16 15:12:35 +0100

    1

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+document.getElementById('menu-open').addEventListener("click", (event) => {
+    document.getElementsByClassName('menu')[0].classList.toggle("open");
+});
+
+document.getElementById('menu-close').addEventListener("click", (event) => {
+    document.getElementsByClassName('menu')[0].classList.toggle("open");
+});
