7e195f431c18893ed0eedb920e989ade4ba514ff
fayerea <fschlief@reacollege.org>
2026-03-17 14:39:33 +0100

    1

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,16 @@
 document.getElementById('menu-open').addEventListener("click", (event) => {
     document.getElementsByClassName('menu')[0].classList.toggle("open");
 });
-
 document.getElementById('menu-close').addEventListener("click", (event) => {
     document.getElementsByClassName('menu')[0].classList.toggle("open");
 });
+
+document.getElementById('see-more-nav').addEventListener("click", (event) => {
+    console.log("click");
+    document.getElementsByClassName('see-more-submenu')[0].classList.toggle("open");
+})
+
+document.getElementById('opleiding').addEventListener("click", (event) => {
+    console.log("click");
+    document.getElementsByClassName('opleiding-submenu')[0].classList.toggle("open");
+})
